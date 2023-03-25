239c2e9352ade029fce959dc7950c049847c83fd
15305966258 <592723369@qq.com>
2023-03-25 18:50:14 +0800

    first

---
+++ src/core/instance/index.js
@@ -0,0 +1,24 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+//，这段代码的作用是确保在非生产环境下使用Vue构造函数时，必须使用new关键字来调用它，
+// 并且在所有情况下都会对Vue实例进行初始化。
+function Vue (options) {
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)  // 定义在
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
